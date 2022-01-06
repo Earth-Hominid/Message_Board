edfb71776ec07d0074b6e3f56acd7a1ec72e96aa
C <earthhominid@gmail.com>
2022-01-06 12:21:07 -0300

    boilerplate added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+
+// Set views as static folder
+app.use(express.static(path.join(__dirname, 'views')));
 
 // when we deploy, server will have port number in env variable so check that first
 const PORT = process.env.PORT || 5000;
