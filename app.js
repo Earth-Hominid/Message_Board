6bc07e35bb46e83cee19d941ca145f299738accf
C <earthhominid@gmail.com>
2022-01-06 22:35:55 -0300

    form functioning

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const { engine } = require('express-handlebars');
-
 const indexRouter = require('./routes/index');
+const formRouter = require('./routes/new');
+const createError = require('http-errors');
 
 const app = express();
 
@@ -15,10 +16,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Homepage
 app.use('/', indexRouter);
+app.use('/new', formRouter);
 
-// Messages API Routes
-app.use('/api/messages', require('./routes/api/messages'));
+// Handle errors
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  // set locals, only providing error in developmen
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // when we deploy, server will have port number in env variable so check that first
 const PORT = process.env.PORT || 5000;
